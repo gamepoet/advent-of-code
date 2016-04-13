a4c1efe2564560bcdebc7900526deabcea3a7890
Ben Scott <gamepoet@gmail.com>
2016-04-12 21:30:16 -0700

    rust: day 1 part 2

--- rust/1/src/main.rs
+++ rust/1/src/main.rs
@@ -5,21 +5,25 @@ use std::path::Path;
 
 fn main() {
   let mut floor = 0;
+  let mut first_basement_index = -1;
 
   let path = Path::new("input.txt");
   let fh = match File::open(&path) {
     Err(why) => panic!("couldn't open {}: {}", path.display(), Error::description(&why)),
     Ok(file) => file,
   };
-  for b in fh.bytes() {
+  for (index, b) in fh.bytes().enumerate() {
     let c = b.unwrap() as char;
     if c == '(' {
       floor += 1;
     }
     else if c == ')' {
       floor -= 1;
+      if first_basement_index == -1 && floor < 0 {
+        first_basement_index = index as i32;
+      }
     }
   }
 
-  println!("floor: {}\n", floor);
+  println!("floor: {}, first_basement_index: {}\n", floor, first_basement_index + 1);
 }
