d068b8f23ed7edfcf9194cf107c92410b48b4779
Ben Scott <gamepoet@gmail.com>
2018-12-01 10:26:36 -0800

    2018 js: day 1, part 2

--- 2018/js/1/index.js
+++ 2018/js/1/index.js
@@ -2,13 +2,22 @@ import fs from 'fs'
 
 function main() {
   const data = fs.readFileSync(process.argv[2]).toString('utf8')
-
   const lines = data.trim().split('\n')
-  let value = 0
-  for (const line of lines) {
-    const line_value = parseInt(line, 10)
 
-    value += line_value
+  const history = new Set()
+  let value = 0
+  let found = false
+  while (!found) {
+    for (const line of lines) {
+      const line_value = parseInt(line, 10)
+      value += line_value
+
+      if (history.has(value)) {
+        found = true
+        break
+      }
+      history.add(value)
+    }
   }
 
   console.log(value)
